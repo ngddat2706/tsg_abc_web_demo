4277557db0c989af1311630b063941efbb9c3fea
Đạt Nguyễn Đình <dinhdat2762000@gmail.com>
2025-10-21 01:21:18 +0700

    add ui payroll planning and function export, import

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "764775399"
+    serviceWorkerVersion: "187189470"
   }
 });

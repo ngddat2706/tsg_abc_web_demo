dd4605b89320db2b91d5463724a63220adeceab2
Đạt Nguyễn Đình <dinhdat2762000@gmail.com>
2025-12-09 08:28:56 +0700

    edit ui dashboard, personnel report

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3750708529"
+    serviceWorkerVersion: "3535683571"
   }
 });

98c6262ce828378b19cf76a3e9ec5351bba4edc5
Đạt Nguyễn Đình <dinhdat2762000@gmail.com>
2026-01-16 13:28:50 +0700

    fix error download icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1475775882"
+    serviceWorkerVersion: "3103594633"
   }
 });

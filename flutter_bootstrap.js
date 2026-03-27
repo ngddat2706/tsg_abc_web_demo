3a89cd686c133250be91ef68a6ace4397d7dd853
Đạt Nguyễn Đình <dinhdat2762000@gmail.com>
2026-03-27 14:53:50 +0700

    fix error 404 icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "15360047"
+    serviceWorkerVersion: "376331163"
   }
 });

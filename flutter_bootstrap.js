6cc5aeed048fdc38fa35b800137e46c6070176ae
Đạt Nguyễn Đình <dinhdat2762000@gmail.com>
2026-04-08 18:21:32 +0700

    add UI function left and new employee, change model table employee

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2943037817"
+    serviceWorkerVersion: "1231813522"
   }
 });

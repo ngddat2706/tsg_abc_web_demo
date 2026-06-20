97ab0d4e12f63a051ab7a4049125a1fd529b4209
Đạt Nguyễn Đình <dinhdat2762000@gmail.com>
2026-06-20 18:35:19 +0700

    fix and update UI personnel screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a1856ea985f0e697644e5b21b3cccbf2",
-"version.json": "eeab2432abee8132ae3f0b2860700b2f",
+const RESOURCES = {"flutter_bootstrap.js": "b5c61257de06cb96f70b1f1cc2dad3de",
+"version.json": "f01e7e555c774d44921cc91dd00da79c",
 "splash/img/light-2x.png": "cd63acdf45784df4a8ccdcef30e866bb",
 "splash/img/dark-4x.png": "7eb545c60728b6822621327d0e2af5b8",
 "splash/img/light-3x.png": "89289fdd69cbaf573b5423ec96074d48",
@@ -14,9 +14,9 @@ const RESOURCES = {"flutter_bootstrap.js": "a1856ea985f0e697644e5b21b3cccbf2",
 "splash/img/dark-1x.png": "b8e64009d8264b93fc4a50b787537f1b",
 "splash/img/light-1x.png": "b8e64009d8264b93fc4a50b787537f1b",
 "favicon.ico": "7ac708ce5ababbf05c84def2547a31ae",
-"index.html": "004680832d9160fc45f3bee5f1e8c665",
-"/": "004680832d9160fc45f3bee5f1e8c665",
-"main.dart.js": "f7fb6dda9b09035eddd80cb184f25240",
+"index.html": "a05c0b20b1e75e7876ca40dc22ba039d",
+"/": "a05c0b20b1e75e7876ca40dc22ba039d",
+"main.dart.js": "b69cee680dadc9b0e649a0d8bac82ada",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "icons/icon-192.png": "ca52ed93ad8ce81400d54951f346b59d",
 "icons/icon-192-maskable.png": "26024c5f347149685d6b58b0d53e5b2d",

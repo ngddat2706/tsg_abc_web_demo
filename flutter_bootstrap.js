21103ff0526f73c3a3cd2fe7653fdffc6cd1a1e2
Vu Trong Thien <thienvt151@gmail.com>
2026-08-07 15:50:49 +0700

    Release Testing v2.2.2608071545

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "172968574"
+    serviceWorkerVersion: "1474135182"
   }
 });

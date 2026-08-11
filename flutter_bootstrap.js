01651765b5def5f137814d1a45eab2381d06ea8d
Vu Trong Thien <thienvt151@gmail.com>
2026-08-11 18:07:22 +0700

    fix state and view display

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2981134806"
+    serviceWorkerVersion: "1221475655"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -66,14 +66,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "55ae36ca270401d1fdd8d93478d93100
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.ico": "7ac708ce5ababbf05c84def2547a31ae",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "07d2c1b279c46d66cd2272edad03f961",
+"flutter_bootstrap.js": "70a9b6922b79a7ddd95df52e5f0afefc",
 "icons/icon-192-maskable.png": "26024c5f347149685d6b58b0d53e5b2d",
 "icons/icon-192.png": "ca52ed93ad8ce81400d54951f346b59d",
 "icons/icon-512-maskable.png": "a8a23c2a1b55324f281200c36240cf87",
 "icons/icon-512.png": "3767892768928be9eff0c3eee6e3cfa6",
-"index.html": "0593c03c3f4442f3fab358002bffc3fd",
-"/": "0593c03c3f4442f3fab358002bffc3fd",
-"main.dart.js": "4eb736608ffc76f4695568cef0f755f3",
+"index.html": "fcdebc04b58790e644a020b01634bf2a",
+"/": "fcdebc04b58790e644a020b01634bf2a",
+"main.dart.js": "ea9cee0816ba4f79f75326cce2cb7fc6",
 "manifest.json": "98a9dd6fb4cc8602a8fbd658e9873130",
 "splash/img/dark-1x.png": "b8e64009d8264b93fc4a50b787537f1b",
 "splash/img/dark-2x.png": "cd63acdf45784df4a8ccdcef30e866bb",
@@ -83,7 +83,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "55ae36ca270401d1fdd8d93478d93100
 "splash/img/light-2x.png": "cd63acdf45784df4a8ccdcef30e866bb",
 "splash/img/light-3x.png": "89289fdd69cbaf573b5423ec96074d48",
 "splash/img/light-4x.png": "7eb545c60728b6822621327d0e2af5b8",
-"version.json": "e7713434c342ae9bc942ddcff3b23e1e"};
+"version.json": "b1fb085fb372a0d4deaeda45099818fb"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",

d0fff9f611ac68054005377b98438ab915daeef7
Đạt Nguyễn Đình <dinhdat2762000@gmail.com>
2026-08-14 22:37:52 +0700

    fix UI follow feedback and change model actual payroll, model payroll year

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "35010970e7e8fd3e343455e432892ba4",
-"version.json": "09e09384b5b390634414e7e59327d7b6",
+const RESOURCES = {"flutter_bootstrap.js": "0612bfbc0eb16b8ed30475b8d3efc7dc",
+"version.json": "5819bceddb96c28ff92e1d8efb32d4de",
 "splash/img/light-2x.png": "cd63acdf45784df4a8ccdcef30e866bb",
 "splash/img/dark-4x.png": "7eb545c60728b6822621327d0e2af5b8",
 "splash/img/light-3x.png": "89289fdd69cbaf573b5423ec96074d48",
@@ -14,9 +14,9 @@ const RESOURCES = {"flutter_bootstrap.js": "35010970e7e8fd3e343455e432892ba4",
 "splash/img/dark-1x.png": "b8e64009d8264b93fc4a50b787537f1b",
 "splash/img/light-1x.png": "b8e64009d8264b93fc4a50b787537f1b",
 "favicon.ico": "7ac708ce5ababbf05c84def2547a31ae",
-"index.html": "146c9c1cf5cd0a696c1c80ff03d9353a",
-"/": "146c9c1cf5cd0a696c1c80ff03d9353a",
-"main.dart.js": "26a62ec70e43fedd369c42aa305e82e8",
+"index.html": "29e0e62255891b97204995b58e1ee6ac",
+"/": "29e0e62255891b97204995b58e1ee6ac",
+"main.dart.js": "620b7113814ba3f80f220aacac63b607",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "icons/icon-192.png": "ca52ed93ad8ce81400d54951f346b59d",
 "icons/icon-192-maskable.png": "26024c5f347149685d6b58b0d53e5b2d",
